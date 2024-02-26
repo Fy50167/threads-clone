3f0b513582998cdb80e035d2c1ce115229701c72
Francis Yang <fy50167@gmail.com>
2024-02-25 17:27:52 -0800

    import constants links

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,3 +1,7 @@
 export default function LeftSidebar() {
-    return <h1>LeftSidebar</h1>;
+    return (
+        <section className='custom-scrollbar leftsidebar'>
+            <div className='flex w-full flex-1 flex-col gap-6 px-6'></div>
+        </section>
+    );
 }
