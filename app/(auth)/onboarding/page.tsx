c344d715bd337a622d01a6ac3bafbb8447d7705f
Francis Yang <fy50167@gmail.com>
2024-02-27 08:45:38 -0800

    shadcn form

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,4 +1,20 @@
+import AccountProfile from '@/components/forms/AccountProfile';
+import { currentUser } from '@clerk/nextjs';
+
 export default async function Onboarding() {
+    const user = await currentUser();
+
+    const userInfo = {};
+
+    const userData = {
+        id: user?.id,
+        objectId: userInfo?.id,
+        username: userInfo?.username || user?.username,
+        name: userInfo?.name || user?.firstName || "",
+        bio: userInfo?bio || '',
+        image: userInfo?.image || user?.imageUrl
+    }
+
     return (
         <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
             <h1 className='head-text'>Onboarding</h1>
@@ -6,7 +22,9 @@ export default async function Onboarding() {
                 Complete your profile to use Threads
             </p>
 
-            <section className='mt-9 bg-dark-2 p-10'></section>
+            <section className='mt-9 bg-dark-2 p-10'>
+                <AccountProfile user = {userData} btnTitle = "Continue"/>
+            </section>
         </main>
     );
 }
