d1a0d3aa1e18e31123c46c3852b2e2c43560f5c4
Francis Yang <fy50167@gmail.com>
2024-02-28 17:31:37 -0800

    create shadcn tabs

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,13 @@
+interface Props {
+    currentUserId: string;
+    accountId: string;
+    accountType: string;
+}
+
+export default async function ThreadsTab({
+    currentUserId,
+    accountId,
+    accountType,
+}: Props) {
+    return <section>ThreadsTab</section>;
+}
