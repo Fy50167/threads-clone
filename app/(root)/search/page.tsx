87c191e042f432b3925da410711fb009b1257154
Francis Yang <fy50167@gmail.com>
2024-03-01 12:04:00 -0800

    get activity function

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,8 +1,6 @@
-import ProfileHeader from '@/components/shared/ProfileHeader';
-import { profileTabs } from '@/constants';
+import UserCard from '@/components/cards/UserCard';
 import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
-import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
@@ -26,6 +24,25 @@ export default async function Page() {
     return (
         <section>
             <h1 className='head-text mb-10'>Search</h1>
+
+            <div className='mt-14 flex flex-col gap-9'>
+                {result.users.length === 0 ? (
+                    <p className='no-result'>No users</p>
+                ) : (
+                    <>
+                        {result.users.map((person) => (
+                            <UserCard
+                                key={person.id}
+                                id={person.id}
+                                name={person.name}
+                                username={person.username}
+                                imgUrl={person.image}
+                                personType='User'
+                            />
+                        ))}
+                    </>
+                )}
+            </div>
         </section>
     );
 }
