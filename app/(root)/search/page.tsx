4f87c133f996b9b3edfb15bdfff8152b280cd67c
Francis Yang <fy50167@gmail.com>
2024-03-01 10:13:00 -0800

    create search, activity, and communities pages

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,24 @@
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import { profileTabs } from '@/constants';
+import { fetchUser } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+
+export default async function Page() {
+    const user = await currentUser();
+
+    if (!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+
+    if (!userInfo?.onboarded) {
+        redirect('/onboarding');
+    }
+
+    return (
+        <section>
+            <h1 className='head-text mb-10'>Search</h1>
+        </section>
+    );
+}
