7e308e3db1d57eb8a62c1e5c5f8a39fb136c97ef
Francis Yang <fy50167@gmail.com>
2024-03-01 12:15:50 -0800

    display all activity

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,5 +1,7 @@
-import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { fetchUser, fetchUsers, getActivity } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 export default async function Page() {
@@ -12,9 +14,39 @@ export default async function Page() {
     if (!userInfo?.onboarded) {
         redirect('/onboarding');
     }
+
+    const activity = await getActivity(userInfo._id);
+
     return (
         <section>
             <h1 className='head-text mb-10'>Activity</h1>
+
+            <section className='mt-10 flex flex-col gap-5'>
+                {activity.length > 0 ? (
+                    <>
+                        {activity.map((activity) => (
+                            <Link
+                                key={activity._id}
+                                href={`/thread/${activity.parentId}`}
+                            >
+                                <article className='activity-card'>
+                                    <Image
+                                        src={activity.author.image}
+                                        alt='profile picture'
+                                        height={20}
+                                        width={20}
+                                        className='rounded-full object-cover'
+                                    />
+                                </article>
+                            </Link>
+                        ))}
+                    </>
+                ) : (
+                    <p className='!text-base-regular text-light-3'>
+                        No activity yet
+                    </p>
+                )}
+            </section>
         </section>
     );
 }
