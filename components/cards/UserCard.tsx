49343a8a8ca28e9adb8012166f582d064f70ae21
Francis Yang <fy50167@gmail.com>
2024-03-01 11:43:15 -0800

    render user card

---
+++ components/cards/UserCard.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+interface Props {
+    id: string;
+    name: string;
+    username: string;
+    imgUrl: string;
+    personType: string;
+}
+
+export default function UserCard({
+    id,
+    name,
+    username,
+    imgUrl,
+    personType,
+}: Props) {
+    return (
+        <article className='user-card'>
+            <div className='user-card_avatar'>
+                <Image
+                    src={imgUrl}
+                    alt='logo'
+                    width={48}
+                    height={48}
+                    className='rounded-full'
+                />
+            </div>
+        </article>
+    );
+}
